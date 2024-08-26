20da950c4a608d26b9435a696c0772ae6a72ce75
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 11:17:18 +0530

    Create style string c++.c

---
+++ style string c++.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std ;
+
+int main (){
+    string greeting1 = "hello world " ;
+    string  greeting2 = "tension mt lo " ;
+    cout << greeting1<< "\n";
+    cout << greeting2 ;
+    return 0 ;
+}
